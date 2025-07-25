dd2be501154444e5aaee008f1f8f37678e26bd5c
shivamawasthi7 <shivam76688awasthi@gmail.com>
2025-07-25 21:30:10 +0530

    day 4

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import"dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+
+//create express app and HTTP server
+const app =express();
+const server =http.createServer(app)
+
+//middleware
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req, res)=> res.send("server is live"));
+
+
+//connect to mongodb
+await connectDB();
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, ()=> console.log("server is running on PORT:" + PORT));
